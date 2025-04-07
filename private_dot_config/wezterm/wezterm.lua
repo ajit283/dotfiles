172d8f9e25e294ca968920ddb3e437f0c9a637f4
Ajit Mistry <ajmistry@nvidia.com>
2025-04-07 22:04:39 +0200

    x

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -24,7 +24,7 @@ config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 -- config.window_background_opacity = 0.9
 config.font_size = 16
-config.line_height = 1.1
+config.line_height = 1.2
 config.mouse_bindings = {
 	-- Left click to move the cursor
 	{
